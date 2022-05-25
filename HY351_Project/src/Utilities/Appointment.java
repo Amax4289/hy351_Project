ed96a4da699eade1aba26952034e63432f6adcc7
Rafail Balaskas <r.balaskas.sd@gmail.com>
2022-05-25 19:06:49 +0300

    last one!!!

---
+++ HY351_Project/src/Utilities/Appointment.java
@@ -0,0 +1,70 @@
+import java.util.Date;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("91a0d092-f50a-4177-9a17-3310090faeed")
+public class Appointment {
+    @objid ("3852f3b0-b4d3-46ea-ab05-85737eae60ee")
+    private Date Date;
+
+    @objid ("b9e7dc84-5584-4ee8-980f-0888fd661552")
+    private String Time;
+
+    @objid ("4701d783-925b-4085-b8e2-dd6b6aa80498")
+    private int Citizen_AMKA;
+
+    @objid ("d9b453b9-4541-4b6b-8233-115d8b3bbeb6")
+    private String Hospital_Name;
+
+    @objid ("00d95238-5e41-4f68-ad94-cd9dd2596411")
+    private String Vaccine_Name;
+
+    @objid ("7847ff87-bbd4-4ee4-a4d9-0e3fe1e4ec65")
+    private int Appointment_ID;
+
+    @objid ("9165201e-1afd-416b-a419-e34519f11356")
+    private String Dose;
+
+    @objid ("d80ea111-5751-4b87-a074-df8f33547721")
+    private int Confirmed;
+
+    @objid ("d70a5fab-33f9-4959-bdd7-dee9dbae1915")
+    private void Set_Date(String Date) {
+    }
+
+    @objid ("88fb1c63-50dc-43f2-9b58-a7e582e035d7")
+    private String Get_Date() {
+    }
+
+    @objid ("75f6d5bb-f2cb-4f69-b8d8-e2c453232130")
+    private String Get_Time() {
+    }
+
+    @objid ("fac972de-cc66-4561-8ddf-b631b76ff335")
+    private void Set_Time(String Time) {
+    }
+
+    @objid ("f953a643-0fa4-4d07-bdc6-98274fc70a29")
+    private void Set_Appointment_ID(int Appointment_ID) {
+    }
+
+    @objid ("540cd317-b5c2-480d-ad09-3789e7930946")
+    private int Get_Appointment_ID() {
+    }
+
+    @objid ("79cc2291-1de7-4c5d-8623-399f49cd200d")
+    private int Get_Dose() {
+    }
+
+    @objid ("bd40e0b0-7158-4cc0-9ce3-f230b378307d")
+    private void Set_Dose(int Dose) {
+    }
+
+    @objid ("5b106552-2d82-4db2-b9e8-4be26f9b2e73")
+    private int Get_Confirmed() {
+    }
+
+    @objid ("21a9deac-bfba-4a27-8524-deb5736b3e8e")
+    private void Set_Confirmed(int Confirmed) {
+    }
+
+}
---
+++ HY351_Project/src/Utilities/Citizen.java
@@ -0,0 +1,148 @@
+import java.util.Date;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("388d201b-ddb9-427d-94b9-ef6db67c1a02")
+public @interface Citizen  {
+    @objid ("b61309ad-c5e2-468b-8586-f80aaa6ecd65")
+    String Fullname();
+
+    @objid ("09b5a0fc-3edc-4dd0-9d75-139a5797dcdb")
+    int Phone();
+
+    @objid ("25742a30-453c-4c61-b00a-71e71c20ff7e")
+    int AMKA();
+
+    @objid ("a38dd5be-4641-4c84-a0a9-11703027d8d8")
+    Date birthdate();
+
+    @objid ("711f2b1e-31e6-4a30-b975-0bd4d2324f75")
+    String Vaccination_State();
+
+    @objid ("2ca6e4a6-b676-4595-8224-f7374c9a398e")
+    String sex();
+
+    @objid ("20a49da4-9529-48cf-b1cb-70fb8c2154aa")
+    String email();
+
+    @objid ("93f249cc-03b7-400d-b6b2-1cdd5332fd4e")
+    int Citizen_ID();
+
+    @objid ("80435040-aa11-4a50-82a0-7665eada227c")
+    String username();
+
+    @objid ("ec4991bc-05c9-4a47-b3cd-8c4220cbcf66")
+    String password();
+
+    @objid ("0c07ab25-5d90-491b-9009-1898fc6ca859")
+    private String Get_Vaccination_State(String Vaccination_state) {
+    }
+
+    @objid ("4430dabb-6f89-4d6a-b8f6-32b02b46a9fe")
+    private void Login() {
+    }
+
+    @objid ("ed14896e-534b-4359-a3c6-c89a82c35b95")
+    private void Register() {
+    }
+
+    @objid ("9c858cf6-f7ca-4e77-85c8-f0cf7797dca7")
+    private void Edit_Profile() {
+    }
+
+    @objid ("fb3454c7-f9bd-4f32-b6a3-a669f270f11c")
+    private void Apply_for_Certificate() {
+    }
+
+    @objid ("c117c477-a843-47e4-8fdf-308eb85564c2")
+    private void Cancel_Appointment() {
+    }
+
+    @objid ("ca86be6e-f336-4214-927c-35f18160d73a")
+    private void Delete_Profile() {
+    }
+
+    @objid ("288e14f9-b782-4413-8ba8-3ca42762ab6b")
+    private void Edit_Appointment() {
+    }
+
+    @objid ("08d20577-1b10-4a11-8d1a-7c9bd5ddcb6f")
+    private void Book_Appointment() {
+    }
+
+    @objid ("3fdd2cc9-37ed-48ef-ab9e-0e23785fdd31")
+    private void Set_Vaccination_State(String Vaccination_state) {
+    }
+
+    @objid ("fd2a5490-c083-4fcb-b1d0-dfde1cf36926")
+    private String Get_Fullname() {
+    }
+
+    @objid ("510d7b7a-d1f0-4022-8d7e-838e04c8f577")
+    private int Get_Phone() {
+    }
+
+    @objid ("6410adda-0a6b-40d3-a7fd-507b130c0941")
+    private int Get_AMKA() {
+    }
+
+    @objid ("72eb4f60-fc02-474c-b8b2-12f97d080d29")
+    private String Get_Birthdate() {
+    }
+
+    @objid ("062493d4-12e8-4046-8c5c-ff3dc69c9987")
+    private String Get_Sex() {
+    }
+
+    @objid ("ef7c622d-6ce0-4d1e-bead-93db86d6c9c9")
+    private String Get_Email() {
+    }
+
+    @objid ("c87abcff-ca6a-4895-9ce1-113208aaf60a")
+    private int Get_Citizen_ID() {
+    }
+
+    @objid ("028bb8f1-d1e4-4011-93d5-c7c41a30b787")
+    private String Get_Username() {
+    }
+
+    @objid ("b2ed130d-4fa9-430f-bf05-bbbf78c62e64")
+    private String Get_Password() {
+    }
+
+    @objid ("8bef9c72-54a1-421e-a438-064a271ae377")
+    private void Set_Fullname(String Fullaname) {
+    }
+
+    @objid ("b145492b-78fa-4621-8c65-53c15dc4daa8")
+    private void Set_phone(int phone) {
+    }
+
+    @objid ("f3acdf9f-b847-4192-829f-b243d6b2ac4b")
+    private void Set_AMKA(int AMKA) {
+    }
+
+    @objid ("55d67a32-a320-4e8b-9421-b0cf8612ddf3")
+    private void Set_Birthdate(String Birthdate) {
+    }
+
+    @objid ("0789824e-9650-430d-926e-7ccb77b6c34f")
+    private void Set_Sex(int Citizen_ID) {
+    }
+
+    @objid ("c294848d-4715-4895-8164-4c564a1db61f")
+    private void Set_Email(String Email) {
+    }
+
+    @objid ("f2d638e3-cb44-4a96-921f-d15cb84cfbc4")
+    private void Set_Citizen_ID(int Citizen_ID) {
+    }
+
+    @objid ("d90905f8-6816-44fb-b1b7-3cf263d0cf25")
+    private void Set_Username(String Username) {
+    }
+
+    @objid ("7f18bae4-defd-40b9-bf09-4780cc16fc34")
+    private void Set_Password(String Password) {
+    }
+
+}
